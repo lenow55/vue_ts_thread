eb955d2d1a684e0fa33aa9af1f2e883ecb8bbf00
Новиков Илья <nov.ilucha@yandex.ru>
2022-04-25 09:00:06 +0300

    воркер должен быть не здесь

--- src/main.ts
+++ src/main.ts
@@ -3,23 +3,6 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 
 const app = createApp(App);
-
-// let n: number = 0
-
-// const worker = new Worker("./workers/fibonachi_worker.ts", {
-//   type: "module",
-// });
-
-// // worker.addEventListener("message", (e) => {
-// //   result.innerHTML = e.data;
-// // });
-
-// worker.addEventListener("error", (e) => {
-//   //result.innerHTML = `message: ${e.message}; filename: ${e.filename}; line: ${e.lineno}`;
-//   console.error(e);
-// });
-
-// worker.runWorker(n);
 app.use(createPinia());
 
 app.mount("#app");
