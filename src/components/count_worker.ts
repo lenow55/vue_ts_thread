5250fb5aebef00ada6c5546dd25622547b2bc2a0
Новиков Илья <nov.ilucha@yandex.ru>
2022-04-25 09:00:38 +0300

    исполнеямый файл воркера (не работает почему-то)

---
+++ src/components/count_worker.ts
@@ -0,0 +1,17 @@
+onmessage = function (e) {
+  const num = e.data;
+  console.log("worker");
+  let temp = 0;
+  while (temp < 9999999999) {
+    temp++;
+  }
+  if (Number.isInteger(parseInt(num))) {
+    while (temp < 9999999999) {
+      temp++;
+    }
+    const result = num + 1;
+    self.postMessage(result);
+  } else {
+    throw new Error("Is not a number");
+  }
+};
